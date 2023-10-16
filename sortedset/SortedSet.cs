c45b547fba8120c358f66fa2a8ff4077330c43ed
Thomas Jones1 <thomas.jones1@students.snow.edu>
2023-10-16 09:24:19 -0600

    Added empty project and tests for SortedSet

---
+++ sortedset/SortedSet.cs
@@ -0,0 +1,13 @@
+﻿namespace sortedset;
+using bst_code;
+
+public interface ISortedSet<T> where T: IComparable<T> {
+    public void Size();
+    public void Add(T item);
+    public T Remove(T item);
+}
+
+public class SortedSet
+{
+
+}
---
+++ tests/SortedSetTests.cs
@@ -0,0 +1,11 @@
+namespace tests;
+using bst_code;
+using sortedset;
+
+public class SortedSetTests {
+    [Test]
+    public void TestAdding()
+    {
+        
+    }
+}
